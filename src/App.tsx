d514a01aac39c201ec349012d04853393e716600
ngocxxu <ngocquach43@gmail.com>
2024-02-25 16:19:17 +0700

    config firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import { Chip, MantineProvider, createTheme } from "@mantine/core";
+import { Chip } from "@mantine/core";
 import "@mantine/core/styles.css";
 
-const theme = createTheme({
-  /** Put your mantine theme override here */
-});
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDbtV9MyYpQRi425QMAhTy8vviUZUwC5dY",
+  authDomain: "ngocquach-portfolio.firebaseapp.com",
+  projectId: "ngocquach-portfolio",
+  storageBucket: "ngocquach-portfolio.appspot.com",
+  messagingSenderId: "304486505637",
+  appId: "1:304486505637:web:73b27701092d93fdbdc14c",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 function App() {
-  return (
-    <MantineProvider theme={theme}>
-      <Chip defaultChecked>Awesome chip</Chip>
-    </MantineProvider>
-  );
+  return <Chip defaultChecked>Awesome chip</Chip>;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { MantineProvider, createTheme } from "@mantine/core";
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </MantineProvider>
   </React.StrictMode>
 );
