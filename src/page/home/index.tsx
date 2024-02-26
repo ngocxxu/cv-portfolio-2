218b55fd4cfd8f17d2986e3cfd6951ce0cb5806f
ngocxxu <ngocquach43@gmail.com>
2024-02-26 23:49:22 +0700

    add typing count

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,7 +1,10 @@
-import { TypeAnimation } from "react-type-animation";
-import { SectionItem } from "../../components/sectionItem";
-import style from "./style.module.css";
-import { DataUser } from "../../data";
+import { TypeAnimation } from 'react-type-animation';
+import Square from '../../assets/logo/round-text.png';
+import { SectionItem } from '../../components/sectionItem';
+import { DataUser } from '../../data';
+import style from './style.module.css';
+import { IconArrowDown } from '@tabler/icons-react';
+import CountUp from 'react-countup';
 
 const Home = () => {
   return (
@@ -16,6 +19,24 @@ const Home = () => {
         />
       </div>
       <p className={style.smallText}>{DataUser.home.smallText}</p>
+      <a className={style.rotate}>
+        <img className={style.image} src={Square} alt='round-text' />
+        <IconArrowDown className={style.arrow} />
+      </a>
+      <div className={style.statistic}>
+        <div>
+          <div>
+            <CountUp decimals={1} decimal=',' end={2.5} duration={20} />+
+          </div>
+          <p>YEARS OF EXPERIENCE</p>
+        </div>
+        <div>
+          <div>
+            <CountUp end={10} />+
+          </div>
+          <p>PROJECTS COMPLETED</p>
+        </div>
+      </div>
     </SectionItem>
   );
 };
