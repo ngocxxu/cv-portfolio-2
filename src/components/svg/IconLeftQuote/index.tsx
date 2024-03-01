817b90f8a476eb1fc79feaf648487dfdf619e992
bono43 <bono43@nexondv.com>
2024-03-01 17:22:58 +0700

    adjust size icon quotes

--- src/components/svg/IconLeftQuote/index.tsx
+++ src/components/svg/IconLeftQuote/index.tsx
@@ -14,8 +14,8 @@ export const IconLeftQuote = ({
       xmlns="http://www.w3.org/2000/svg"
       x="0px"
       y="0px"
-      width="20px"
-      height="20px"
+      width="25px"
+      height="25px"
       viewBox="0 0 123.961 123.961"
     >
       <g>
--- src/components/svg/IconRightQuote/index.tsx
+++ src/components/svg/IconRightQuote/index.tsx
@@ -14,8 +14,8 @@ export const IconRightQuote = ({
       xmlns="http://www.w3.org/2000/svg"
       x="0px"
       y="0px"
-      width="20px"
-      height="20px"
+      width="25px"
+      height="25px"
       viewBox="0 0 975.036 975.036"
     >
       <g>
