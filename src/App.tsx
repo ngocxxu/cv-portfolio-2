17bb20152bb15bdaf642cbb8b2646837f17a7f7d
ngocxxu <ngocquach43@gmail.com>
2024-07-15 00:25:10 +0700

    fix: remove carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import "@mantine/core/styles.css";
-import "@mantine/carousel/styles.css";
 import "@mantine/notifications/styles.css";
 import { initializeApp } from "firebase/app";
 import { useRoutes } from "react-router-dom";
